cfadf6b0c6327c10c4b902c5cb42e66a78dd5ef6
Conor Kelly <conor.kelly.irishlife@gmail.com>
2022-04-09 00:25:12 +0100

    Day 2 Finished

--- pages/monkeys.tsx
+++ pages/monkeys.tsx
@@ -29,95 +29,107 @@ const Monkeys: NextPage = () => {
         </div>
         <main className="mt-2 inline-flex max-w-full flex-wrap justify-center">
           {masters.map((master, key) => (
-            <table
-              className="text-blueGray-700 mx-5 mb-6 max-w-full border-collapse flex-col items-center break-words rounded bg-white shadow-lg"
-              id={master.id}
-            >
-              <thead className="thead-light ">
-                <tr>
-                  <th className="bg-blueGray-50 text-blueGray-500 border-blueGray-100 whitespace-nowrap border border-l-0 border-r-0 border-solid px-6 py-3 text-left align-middle text-xs font-semibold uppercase">
-                    {master.name}
-                  </th>
+            <>
+              <table
+                className="text-blueGray-700 mx-5 mb-6 max-w-full border-collapse flex-col items-center break-words rounded bg-white shadow-lg"
+                id={master.id}
+              >
+                <thead className="thead-light ">
+                  <tr>
+                    <th className="bg-blueGray-50 text-blueGray-500 border-blueGray-100 whitespace-nowrap border border-l-0 border-r-0 border-solid px-6 py-3 text-left align-middle text-xs font-semibold uppercase">
+                      {master.name}
+                    </th>
 
-                  <th className="bg-blueGray-50 text-blueGray-500 border-blueGray-100 whitespace-nowrap border border-l-0 border-r-0 border-solid px-6 py-3 text-left align-middle text-xs font-semibold uppercase"></th>
-                  <th className="bg-blueGray-50 text-blueGray-700 border-blueGray-100 min-w-140-px whitespace-nowrap border border-l-0 border-r-0 border-solid px-6 py-3 text-left align-middle text-xs font-semibold uppercase"></th>
-                </tr>
-              </thead>
-              <tbody>
-                <tr>
-                  <th className="whitespace-nowrap border-t-0 border-l-0 border-r-0 p-4 px-6 text-left align-middle text-xs">
-                    {master.golfer1}
-                  </th>
-                  <td className="whitespace-nowrap border-t-0 border-l-0 border-r-0 p-4 px-6 align-middle text-xs ">
-                    {master.g1odds}
-                  </td>
-                  <td className="whitespace-nowrap border-t-0 border-l-0 border-r-0 p-4 px-6 align-middle text-xs ">
-                    {master.g1points}
-                  </td>
-                </tr>
-                <tr>
-                  <th className="whitespace-nowrap border-t-0 border-l-0 border-r-0 p-4 px-6 text-left align-middle text-xs">
-                    {master.golfer2}
-                  </th>
-                  <td className="whitespace-nowrap border-t-0 border-l-0 border-r-0 p-4 px-6 align-middle text-xs ">
-                    {master.g2odds}
-                  </td>
-                  <td className="whitespace-nowrap border-t-0 border-l-0 border-r-0 p-4 px-6 align-middle text-xs ">
-                    {master.g2points}
-                  </td>
-                </tr>
-                <tr>
-                  <th className="whitespace-nowrap border-t-0 border-l-0 border-r-0 p-4 px-6 text-left align-middle text-xs">
-                    {master.golfer3}
-                  </th>
-                  <td className="whitespace-nowrap border-t-0 border-l-0 border-r-0 p-4 px-6 align-middle text-xs ">
-                    {master.g3odds}
-                  </td>
-                  <td className="whitespace-nowrap border-t-0 border-l-0 border-r-0 p-4 px-6 align-middle text-xs ">
-                    {master.g3points}
-                  </td>
-                </tr>
-                <tr>
-                  <th className="whitespace-nowrap border-t-0 border-l-0 border-r-0 p-4 px-6 text-left align-middle text-xs ">
-                    {master.golfer4}
-                  </th>
-                  <td className="whitespace-nowrap border-t-0 border-l-0 border-r-0 p-4 px-6 align-middle text-xs ">
-                    {master.g4odds}
-                  </td>
-                  <td className="whitespace-nowrap border-t-0 border-l-0 border-r-0 p-4 px-6 align-middle text-xs ">
-                    {master.g4points}
-                  </td>
-                </tr>
-                <tr>
-                  <th className="whitespace-nowrap border-t-0 border-l-0 border-r-0 p-4 px-6 text-left align-middle text-xs ">
-                    {master.golfer5}
-                  </th>
-                  <td className="whitespace-nowrap border-t-0 border-l-0 border-r-0 p-4 px-6 align-middle text-xs ">
-                    {master.g5odds}
-                  </td>
-                  <td className="whitespace-nowrap border-t-0 border-l-0 border-r-0 p-4 px-6 align-middle text-xs ">
-                    {master.g5points}
-                  </td>
-                </tr>
-              </tbody>
+                    <th className="bg-blueGray-50 text-blueGray-500 border-blueGray-100 whitespace-nowrap border border-l-0 border-r-0 border-solid px-6 py-3 text-left align-middle text-xs font-semibold uppercase"></th>
+                    <th className="bg-blueGray-50 text-blueGray-700 border-blueGray-100 min-w-140-px whitespace-nowrap border border-l-0 border-r-0 border-solid px-6 py-3 text-left align-middle text-xs font-semibold uppercase"></th>
+                  </tr>
+                </thead>
+                <tbody>
+                  <tr>
+                    <th className="whitespace-nowrap border-t-0 border-l-0 border-r-0 p-4 px-6 text-left align-middle text-xs">
+                      {master.golfer1}
+                    </th>
+                    <td className="whitespace-nowrap border-t-0 border-l-0 border-r-0 p-4 px-6 align-middle text-xs ">
+                      {master.g1odds}
+                    </td>
+                    <td className="whitespace-nowrap border-t-0 border-l-0 border-r-0 p-4 px-6 align-middle text-xs ">
+                      {master.g1points}
+                    </td>
+                  </tr>
+                  <tr>
+                    <th className="whitespace-nowrap border-t-0 border-l-0 border-r-0 p-4 px-6 text-left align-middle text-xs">
+                      {master.golfer2}
+                    </th>
+                    <td className="whitespace-nowrap border-t-0 border-l-0 border-r-0 p-4 px-6 align-middle text-xs ">
+                      {master.g2odds}
+                    </td>
+                    <td className="whitespace-nowrap border-t-0 border-l-0 border-r-0 p-4 px-6 align-middle text-xs ">
+                      {master.g2points}
+                    </td>
+                  </tr>
+                  <tr>
+                    <th className="whitespace-nowrap border-t-0 border-l-0 border-r-0 p-4 px-6 text-left align-middle text-xs">
+                      {master.golfer3}
+                    </th>
+                    <td className="whitespace-nowrap border-t-0 border-l-0 border-r-0 p-4 px-6 align-middle text-xs ">
+                      {master.g3odds}
+                    </td>
+                    <td className="whitespace-nowrap border-t-0 border-l-0 border-r-0 p-4 px-6 align-middle text-xs ">
+                      {master.g3points}
+                    </td>
+                  </tr>
+                  <tr>
+                    <th className="whitespace-nowrap border-t-0 border-l-0 border-r-0 p-4 px-6 text-left align-middle text-xs ">
+                      {master.golfer4}
+                    </th>
+                    <td className="whitespace-nowrap border-t-0 border-l-0 border-r-0 p-4 px-6 align-middle text-xs ">
+                      {master.g4odds}
+                    </td>
+                    <td className="whitespace-nowrap border-t-0 border-l-0 border-r-0 p-4 px-6 align-middle text-xs ">
+                      {master.g4points}
+                    </td>
+                  </tr>
+                  <tr>
+                    <th className="whitespace-nowrap border-t-0 border-l-0 border-r-0 p-4 px-6 text-left align-middle text-xs ">
+                      {master.golfer5}
+                    </th>
+                    <td className="whitespace-nowrap border-t-0 border-l-0 border-r-0 p-4 px-6 align-middle text-xs ">
+                      {master.g5odds}
+                    </td>
+                    <td className="whitespace-nowrap border-t-0 border-l-0 border-r-0 p-4 px-6 align-middle text-xs ">
+                      {master.g5points}
+                    </td>
+                  </tr>
+                  <tr className="bg-red-100">
+                    <th className="whitespace-nowrap border-t-0 border-l-0 border-r-0 p-4 px-6 text-left align-middle text-xs text-red-900 ">
+                      Cut Penalty
+                    </th>
+                    <td className="whitespace-nowrap border-t-0 border-l-0 border-r-0 p-4 px-6 align-middle text-xs "></td>
+                    <td className="whitespace-nowrap border-t-0 border-l-0 border-r-0 p-4 px-6 align-middle text-xs text-red-900">
+                      {master.cutPenalty}
+                    </td>
+                  </tr>
+                </tbody>
 
-              <tfoot className="thead-light ">
-                <tr>
-                  <th className="bg-blueGray-50 text-blueGray-500 border-blueGray-100 whitespace-nowrap border border-l-0 border-r-0 border-solid px-6 py-3 text-left align-middle text-xs font-semibold uppercase">
-                    Total
-                  </th>
+                <tfoot className="thead-light ">
+                  <tr>
+                    <th className="bg-blueGray-50 text-blueGray-500 border-blueGray-100 whitespace-nowrap border border-l-0 border-r-0 border-solid px-6 py-3 text-left align-middle text-xs font-semibold uppercase">
+                      Total
+                    </th>
 
-                  <th className="bg-blueGray-50 text-blueGray-500 border-blueGray-100 whitespace-nowrap border border-l-0 border-r-0 border-solid px-6 py-3 text-left align-middle text-xs font-semibold uppercase"></th>
-                  <th className="bg-blueGray-50 text-blueGray-700 border-blueGray-100 min-w-140-px whitespace-nowrap border border-l-0 border-r-0 border-solid px-6 py-3 text-left align-middle text-xs font-semibold uppercase">
-                    {master.g1points +
-                      master.g2points +
-                      master.g3points +
-                      master.g4points +
-                      master.g5points}
-                  </th>
-                </tr>
-              </tfoot>
-            </table>
+                    <th className="bg-blueGray-50 text-blueGray-500 border-blueGray-100 whitespace-nowrap border border-l-0 border-r-0 border-solid px-6 py-3 text-left align-middle text-xs font-semibold uppercase"></th>
+                    <th className="bg-blueGray-50 text-blueGray-700 border-blueGray-100 min-w-140-px whitespace-nowrap border border-l-0 border-r-0 border-solid px-6 py-3 text-left align-middle text-xs font-semibold uppercase">
+                      {master.g1points +
+                        master.g2points +
+                        master.g3points +
+                        master.g4points +
+                        master.g5points +
+                        master.cutPenalty}
+                    </th>
+                  </tr>
+                </tfoot>
+              </table>
+            </>
           ))}
         </main>
         <footer className="flex h-24 w-full items-center justify-center border-t">
